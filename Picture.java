393c83e6463f8e1bc25f684762d1b129da3ad89a
Sergio <sergio.rodpri@gmail.com>
2024-10-08 09:18:33 +0200

    Ahora se puede ver tambien la luna

--- Picture.java
+++ Picture.java
@@ -15,7 +15,8 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
-
+    private Circle moon;
+    
     /**
      * Constructor for objects of class Picture
      */
@@ -54,6 +55,14 @@ public class Picture
         sun.moveVertical(-40);
         sun.changeSize(80);
         sun.makeVisible();
+        
+        moon = new Circle();
+        moon.makeVisible();
+        moon.moveHorizontal(-160);
+        moon.moveVertical(-20);
+        moon.changeSize(40);
+        moon.changeColor("magenta");
+        
     }
 
     /**
